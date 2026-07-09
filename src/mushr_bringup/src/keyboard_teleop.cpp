7c0e4d0d6dd90362e087cd02f926354d40b4d127
tarunja1ks <tarunjaiks@gmail.com>
2026-07-08 17:57:58 -0700

    changing port name

--- src/mushr_bringup/src/keyboard_teleop.cpp
+++ src/mushr_bringup/src/keyboard_teleop.cpp
@@ -66,7 +66,7 @@ namespace KeyboardTeleop{
             ackermann_pub.publish(msg);
 
             if(quit){
-                ros::Duration(0.1).sleep(); // let the stop command flush before shutting down
+                ros::Duration(0.1).sleep(); 
                 ros::shutdown();
                 break;
             }
